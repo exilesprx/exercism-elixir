7bdae5014f925f629e51522e241e6f8587e07800
Exiles.prx <exiles.prx@gmail.com>
2025-07-25 17:55:26 -0400

    solved exercise secrets

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,29 +1,43 @@
 defmodule Secrets do
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    fn p ->
+      p + secret
+    end
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    fn p ->
+      p - secret
+    end
   end
 
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    fn p ->
+      p * secret
+    end
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    fn p ->
+      trunc(p / secret)
+    end
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    fn p ->
+      Bitwise.band(p, secret)
+    end
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    fn p ->
+      Bitwise.bxor(p, secret)
+    end
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    fn p ->
+      secret_function2.(secret_function1.(p))
+    end
   end
 end
